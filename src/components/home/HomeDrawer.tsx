f445bfc935f265e2ca111c2ecf416806ed4715e1
Chung Tu <tuthanhtrung2000@gmail.com>
2024-04-21 21:56:23 +0700

    feat: add socials url

--- src/components/home/HomeDrawer.tsx
+++ src/components/home/HomeDrawer.tsx
@@ -1,6 +1,6 @@
-'use client';
-import { supabase } from '@/function/supabaseClients';
-import { getTokensData } from '@/supabase/getTokensData';
+"use client";
+import { supabase } from "@/function/supabaseClients";
+import { getTokensData } from "@/supabase/getTokensData";
 import {
   Drawer,
   DrawerBody,
@@ -21,8 +21,8 @@ import {
   Link,
   InputAddon,
   Box,
-} from '@chakra-ui/react';
-import React from 'react';
+} from "@chakra-ui/react";
+import React from "react";
 
 interface HomeDrawerProps {
   isOpen: boolean;
@@ -31,108 +31,183 @@ interface HomeDrawerProps {
 }
 
 function HomeDrawer({ isOpen, onClose, data }: HomeDrawerProps) {
-  const bg = useColorModeValue('light.bg', 'dark.bg');
-  const border = useColorModeValue('light.border', 'dark.border');
+  const bg = useColorModeValue("light.bg", "dark.bg");
+  const border = useColorModeValue("light.border", "dark.border");
   const text = useColorModeValue(
-    'var(--chakra-colors-blackAlpha-500)',
-    'gray.200'
+    "var(--chakra-colors-blackAlpha-500)",
+    "gray.200"
   );
-  const text2 = useColorModeValue('light.text', 'dark.text');
+  const text2 = useColorModeValue("light.text", "dark.text");
   return (
     <>
-      <Drawer isOpen={isOpen} placement='right' onClose={onClose} size={'lg'}>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        size={"lg"}
+      >
         <DrawerOverlay />
-        <DrawerContent backgroundColor={bg} paddingX={'24px'} paddingY={'32px'}>
+        <DrawerContent
+          backgroundColor={bg}
+          paddingX={"24px"}
+          paddingY={"32px"}
+        >
           <DrawerBody>
-            <Flex columnGap={'12px'} marginBottom={'22px'}>
+            <Flex
+              columnGap={"12px"}
+              marginBottom={"22px"}
+            >
               <Image
-                src={`${data?.imageUrl}` || '/image/token-image-2.png'}
-                width={'86px'}
-                height={'86px'}
-                objectFit={'cover'}
-                objectPosition={'center'}
+                src={`${data?.imageUrl}` || "/image/token-image-2.png"}
+                width={"86px"}
+                height={"86px"}
+                objectFit={"cover"}
+                objectPosition={"center"}
               />
-              <Flex flexDirection={'column'} rowGap={'4px'} overflow={'hidden'}>
-                <Text fontSize={'24px'} lineHeight={'32px'}>
+              <Flex
+                flexDirection={"column"}
+                rowGap={"4px"}
+                overflow={"hidden"}
+              >
+                <Text
+                  fontSize={"24px"}
+                  lineHeight={"32px"}
+                >
                   {data?.BaseTokenName}
                 </Text>
-                <Text color={text2} fontSize={'16px'} lineHeight={'24px'}>
+                <Text
+                  color={text2}
+                  fontSize={"16px"}
+                  lineHeight={"24px"}
+                >
                   {data?.BaseTokenSymbol}
                 </Text>
                 <Link
                   noOfLines={1}
-                  textOverflow={'ellipsis'}
-                  textDecoration={'underline'}
+                  textOverflow={"ellipsis"}
+                  textDecoration={"underline"}
                   href={`https://solscan.io/token/${data?.BaseTokenAddress}`}
                   isExternal
                   color={text2}
-                  fontSize={'14px'}
-                  lineHeight={'20px'}
+                  fontSize={"14px"}
+                  lineHeight={"20px"}
                   _hover={{ opacity: 0.5 }}
                 >
                   {data?.BaseTokenAddress}
                 </Link>
-                <Flex marginTop={'8px'}></Flex>
+                <Flex
+                  marginTop={"8px"}
+                  alignItems={"center"}
+                  columnGap={"12px"}
+                >
+                  {data?.typesocials_Pair && (
+                    <Link
+                      href={data?.urlsocials_Pair}
+                      isExternal
+                    >
+                      <Image
+                        src={`/image/social/${data?.typesocials_Pair}.png`}
+                        width={"32px"}
+                        height={"32px"}
+                      />
+                    </Link>
+                  )}
+                  {data?.typesocials && (
+                    <Link
+                      href={data?.urlsocials}
+                      isExternal
+                    >
+                      <Image
+                        src={`/image/social/${data?.typesocials}.png`}
+                        width={"32px"}
+                        height={"32px"}
+                      />
+                    </Link>
+                  )}
+                  {data?.urlweb && (
+                    <Link
+                      href={data?.urlweb}
+                      isExternal
+                    >
+                      <Image
+                        src="/image/social/web.png"
+                        width={"32px"}
+                        height={"32px"}
+                      />
+                    </Link>
+                  )}
+                </Flex>
               </Flex>
             </Flex>
             <Flex
-              borderTop={'1px solid'}
+              borderTop={"1px solid"}
               borderTopColor={border}
-              paddingTop={'16px'}
-              flexDirection={'column'}
-              rowGap={'16px'}
+              paddingTop={"16px"}
+              flexDirection={"column"}
+              rowGap={"16px"}
             >
               <Box>
                 <InputGroup>
                   <Input
-                    type='number'
-                    placeholder='Enter amount to send (per like)'
-                    fontSize={'16px'}
+                    type="number"
+                    placeholder="Enter amount to send (per like)"
+                    fontSize={"16px"}
                     _placeholder={{
-                      color: 'gray.400',
+                      color: "gray.400",
                     }}
                   />
                   <InputRightAddon>
-                    <Flex columnGap={'10px'}>
+                    <Flex columnGap={"10px"}>
                       <Image
-                        src={`${data?.imageUrl}` || '/image/token-image-2.png'}
-                        width='20px'
-                        height={'20px'}
+                        src={`${data?.imageUrl}` || "/image/token-image-2.png"}
+                        width="20px"
+                        height={"20px"}
                       />
-                      <Text fontSize={'16px'} lineHeight={'24px'}>
+                      <Text
+                        fontSize={"16px"}
+                        lineHeight={"24px"}
+                      >
                         {data?.BaseTokenSymbol}
                       </Text>
                     </Flex>
                   </InputRightAddon>
                 </InputGroup>
-                <Text fontSize={'11px'} lineHeight={'19px'} color={text}>
+                <Text
+                  fontSize={"11px"}
+                  lineHeight={"19px"}
+                  color={text}
+                >
                   with per like, you will need to pay ~0.00205 sol to solana
                   network
                 </Text>
               </Box>
               <InputGroup>
                 <Input
-                  type='number'
-                  placeholder='Enter amount to like (min: 10)'
+                  type="number"
+                  placeholder="Enter amount to like (min: 10)"
                   _placeholder={{
-                    color: 'gray.400',
+                    color: "gray.400",
                   }}
                 />
                 <InputRightAddon>
-                  <Image src='/image/thumbup.svg' />
+                  <Image src="/image/thumbup.svg" />
                 </InputRightAddon>
               </InputGroup>
               <Button
-                backgroundColor={'blue.500'}
-                color={'white'}
-                _hover={{ backgroundColor: 'blue.500', opacity: 0.8 }}
+                backgroundColor={"blue.500"}
+                color={"white"}
+                _hover={{ backgroundColor: "blue.500", opacity: 0.8 }}
                 onClick={async () => {
                   getTokensData();
                 }}
               >
                 LIKE FOR DAI
               </Button>
-              <Text color={text} fontSize={'16px'} lineHeight={'24px'}>
+              <Text
+                color={text}
+                fontSize={"16px"}
+                lineHeight={"24px"}
+              >
                 A node is a computer that connects to a cryptocurrency network.
                 The node supports the cryptocurrency's network through either
                 relaying transactions, validation, or hosting a copy of the
@@ -140,7 +215,11 @@ function HomeDrawer({ isOpen, onClose, data }: HomeDrawerProps) {
                 computer (node) has a copy of the blockchain of the
                 cryptocurrency it supports.
               </Text>
-              <Text color={text} fontSize={'16px'} lineHeight={'24px'}>
+              <Text
+                color={text}
+                fontSize={"16px"}
+                lineHeight={"24px"}
+              >
                 When a transaction is made, the node creating the transaction
                 broadcasts details of the transaction using encryption to other
                 nodes throughout the node network so that the transaction (and
